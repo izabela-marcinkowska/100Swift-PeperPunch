ca2c72100433ab4404c0cfb1b62fe198de6cf862
Devy <izabela.marcinkowska@outlook.com>
2024-09-02 19:20:44 +0200

    feat: Add logic of the game

--- PaperPunch/ContentView.swift
+++ PaperPunch/ContentView.swift
@@ -7,37 +7,67 @@
 
 import SwiftUI
 
+let choices = ["scissors", "paper", "rock"]
+var result = ""
 struct ContentView: View {
-    let choices = ["scissors", "papper", "rock"]
-    @State var userChoice = ""
+    @State private var userChoice = ""
     
     var body: some View {
         VStack {
             Button {
                 userChoice = "scissors"
+                playGame(choice: userChoice)
             } label: {
                 Text("scissors")
             }
             Button {
                 userChoice = "paper"
+                playGame(choice: userChoice)
             } label: {
                 Text("paper")
             }
             Button {
                 userChoice = "rock"
+                playGame(choice: userChoice)
             } label: {
                 Text("rock")
             }
             
             Text("Computer Choice is \(getComputerChoice(choices))")
             Text("User Choice is \(userChoice)")
+            Text("Result \(result)")
+            Button {
+                userChoice = ""
+            } label: {
+                Text("play again")
+            }
         }
     }
+    func playGame (choice: String) -> Void {
+        var computersChoice = getComputerChoice(choices)
+        if (choice == "rock" && computersChoice == "paper") {
+            result = "you loose"
+        } else if (choice == "paper" && computersChoice == "rock") {
+            result = "you win"
+        } else if (choice == "scissors" && computersChoice == "rock") {
+            result = "you loose"
+        } else if (choice == "rock" && computersChoice == "scissors") {
+            result = "you win"
+        } else if (choice == "paper" && computersChoice == "scissors") {
+            result = "you loose"
+        } else if (choice == "scissors" && computersChoice == "paper") {
+            result = "you win"
+        } else {
+            result = "tie"
+        }
+    }
+
 }
 
 func getComputerChoice (_ choices: [String]) -> String {
     choices.randomElement()!
 }
+
 
 #Preview {
     ContentView()
