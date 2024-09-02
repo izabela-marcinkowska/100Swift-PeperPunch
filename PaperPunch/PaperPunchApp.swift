66d81cb69d11103ce9290c34eef4269e71c80f9b
Devy <izabela.marcinkowska@outlook.com>
2024-09-02 09:10:51 +0200

    First commit

---
+++ PaperPunch/PaperPunchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaperPunchApp.swift
+//  PaperPunch
+//
+//  Created by Izabela Marcinkowska on 2024-09-02.
+//
+
+import SwiftUI
+
+@main
+struct PaperPunchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
